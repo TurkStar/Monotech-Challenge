48fbc146d10d170477689543f42887ebad234477
srkakgul@gmail.com <s.akgul@amatis.nl>
2022-05-17 14:42:29 +0300

    completed

--- app/Http/Controllers/Api/PromotionCodesController.php
+++ app/Http/Controllers/Api/PromotionCodesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Promotion;
+use Illuminate\Support\Facades\DB;
 
 class PromotionCodesController extends Controller
 {
@@ -50,6 +51,70 @@ class PromotionCodesController extends Controller
             'success' => true,
             'data'   => $data
           )); 
+    }
+
+    public function assignPromotion(Request $request) {
+
+
+        $promotion = Promotion::where('code', $request->code)->get();
+
+        if ($promotion->isEmpty()) {
+            return response()->json('Promotion not found', 404);
+        }
+
+        $quata_count = DB::table('user_promotions')
+                            ->select('id')
+                            ->where('promotion_id', $promotion[0]->id)
+                            ->count();
+
+        
+        if ($quata_count + 1 > $promotion[0]->quota) {
+            return "Promotion quota is full";
+        }
+        
+        // Check if promotion already used
+        $promotion_exist = DB::table('user_promotions')
+        ->select('user_id')
+        ->where('promotion_id', $promotion[0]->id)
+        ->first();
+
+        if ($promotion_exist) {
+            if ($promotion_exist->user_id == $request->user_id) {
+                return response()->json('Promotion already used', 403);
+            }
+        }  
+
+        // Add promotion to user
+        DB::table('user_promotions')->insert([
+            'user_id' => $request->user_id,
+            'promotion_id' => $promotion[0]->id
+        ]);
+        
+        $current_balance = DB::table('wallets')
+                             ->select('balance')
+                             ->where('user_id', $request->user_id)
+                             ->get();
+                             
+        // If user does not have a wallet
+        if ($current_balance->isEmpty()) {
+
+            DB::table('wallets')->updateOrInsert(
+                ['user_id' => $request->user_id], ['balance' => $promotion[0]->amount]
+            );
+
+            return Response()->json(array(
+                'success' => true
+             ));
+        }
+        
+        DB::table('wallets')->updateOrInsert(
+            ['user_id' => $request->user_id], ['balance' => $current_balance[0]->balance + $promotion[0]->amount]
+        );
+ 
+        return Response()->json(array(
+            'success' => true
+          )); 
+
     }
     
 }
--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,7 @@ use App\Http\Controllers\Api\PromotionCodesController;
     Route::get('/promotion-codes/{id}', [PromotionCodesController::class, 'show']);
     Route::get('/promotion-codes', [PromotionCodesController::class, 'index']);
     Route::post('/promotion-codes', [PromotionCodesController::class, 'store']);
+    Route::post('/assign-promotion', [PromotionCodesController::class, 'assignPromotion']);
+   
 
 
